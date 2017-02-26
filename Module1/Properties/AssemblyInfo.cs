a230be6c8e0e6720201fd966693e4de6b463b5ee
Richard VanSickle <richard.vansickle@freedomvoice.com>
2017-02-25 18:10:25 -0800

    Even More Cleanup

--- Module1/Properties/AssemblyInfo.cs
+++ Module1/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("cSharpTraining")]
+[assembly: AssemblyTitle("Module1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("cSharpTraining")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Module1")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5010a362-a86c-45c3-8bff-3be02670e2a6")]
+[assembly: Guid("383e7989-3832-421f-a579-13080ab39753")]
 
 // Version information for an assembly consists of the following four values:
 //
